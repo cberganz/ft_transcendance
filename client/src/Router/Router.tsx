c2772793cdfe8c6477563fd572b816abb8e0fb8a
rbicanic <robin.bianic@gmail.com>
2023-01-14 08:19:42 +0100

    ad alertPopup + secure passwords

--- client/src/Router/Router.tsx
+++ client/src/Router/Router.tsx
@@ -14,6 +14,7 @@ import { selectCurrentUser } from '../Hooks/authSlice'
 import { selectCurrentToken } from '../Hooks/authSlice'
 import { useSelector } from "react-redux"
 import io from "socket.io-client";
+import AlertPopup from '../Components/AlertPopup';
 
 export const usersStatusSocket = io("http://localhost:3000/app");
 
@@ -31,6 +32,7 @@ function	OutletRoute() {
 		<div>
 			<PrimarySearchAppBar />
 			<Outlet/>
+			<AlertPopup/>
 		</div>
 	)
 }
